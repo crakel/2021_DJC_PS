381d891f3492a07b99fb411161a8e2ee7f22c30e
김기윤 <crakel528@ajou.ac.kr>
2022-06-14 02:52:08 +0900

    add 2294_동전 2, 2812_크게 만들기

---
+++ 2022_Summer/백준/DP/2294_동전 2.py
@@ -0,0 +1,19 @@
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+li = []
+
+for i in range(n):
+    li.append(int(sys.stdin.readline().rstrip()))
+
+dp = [10001] * (k + 1)
+dp[0] = 0
+
+for num in li:
+    for i in range(num, k + 1):
+        dp[i] = min(dp[i], dp[i - num] + 1)
+if dp[k] == 10001:
+    print(-1)
+else:
+    print(dp[k])
